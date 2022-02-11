86663280760f55c1aa967182209ae44f29eceaa9
yassineelhoubi <elhoubiyassine@gmail.com>
2022-02-11 20:40:16 +0100

    add project

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 const Navigation = dynamic(() => import("../components/Navigation"));
 const Greetings = dynamic(() => import("../containers/Greetings"));
 const Skills = dynamic(() => import("../containers/Skills"));
-
+const Projects = dynamic(() => import("../containers/Projects"));
 export default function Home() {
   return (
     <>
     <Navigation />
     <Greetings/>
     <Skills />
+    <Projects/>
     </>
     
   )
